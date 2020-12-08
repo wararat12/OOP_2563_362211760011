e643739ed0c6be8e9860b5b24c83b797b1146124
LabComMT-3 <numphuingza@gmail.com>
2020-12-08 11:05:26 +0700

    Initial commit

---
+++ src/LAB1/Helloworld.java
@@ -0,0 +1,21 @@
+package LAB1;
+
+public class Helloworld  {
+
+    public static void main(String[] args) {
+     System.out.println("Hello world");
+
+
+    }//main
+
+
+
+
+
+
+
+
+
+
+
+}//class
