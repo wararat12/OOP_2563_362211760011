0560b2eef0a124b48c127508a79a0385e37d1038
Wararat Parsit <75650603+wararat12@users.noreply.github.com>
2020-12-15 08:48:56 +0700

    Update Helloworld.java

--- src/LAB1/Helloworld.java
+++ src/LAB1/Helloworld.java
@@ -4,6 +4,8 @@ public class Helloworld  {
 
     public static void main(String[] args) {
      System.out.println("Hello world");
+         System.out.println("Hello world");
+         System.out.println("Hello world");
 
 
     }//main
